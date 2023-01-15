2b9d65ac001a6175e0d944ad7c9aca8851b2c97a
Michael <mpattison80@gmail.com>
2023-01-15 14:28:53 +0000

    tag v0.0.3

--- custom_components/waterkotte_heatpump/const.py
+++ custom_components/waterkotte_heatpump/const.py
@@ -3,7 +3,7 @@
 NAME = "Waterkotte Heatpump"
 DOMAIN = "waterkotte_heatpump"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 TITLE = "Waterkotte"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
 ISSUE_URL = "https://github.com/pattisonmichael/waterkotte-heatpump/issues"
